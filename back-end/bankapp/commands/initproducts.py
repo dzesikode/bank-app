6a6cd0c30f35ff9caee1006b582032f6be172f74
dzes483 <jban@sdf.org>
2021-04-14 21:01:56 +0300

    Add command to create default products

---
+++ back-end/bankapp/commands/initproducts.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from bankapp.models import Product
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if Product.objects.count() == 0:
+            for default_product in settings.DEFAULT_PRODUCTS:
+                print(f'Creating product {default_product.name}...')
+                new_product = Product.objects.create(name=default_product.name,
+                                                     age=default_product.age,
+                                                     student=default_product.student,
+                                                     income=default_product.income)
+                new_product.save()
+        else:
+            print('Products already exist.')
+
