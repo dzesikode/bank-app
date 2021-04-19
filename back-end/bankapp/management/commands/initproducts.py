b2b70daa46a589b3dc1089f32a9ca6c8404e1969
dzes483 <jban@sdf.org>
2021-04-19 21:13:02 +0300

    Fix dict keys

--- back-end/bankapp/management/commands/initproducts.py
+++ back-end/bankapp/management/commands/initproducts.py
@@ -8,11 +8,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if Product.objects.count() == 0:
             for default_product in settings.DEFAULT_PRODUCTS:
-                print(f'Creating product {default_product.name}...')
-                new_product = Product.objects.create(name=default_product.name,
-                                                     age=default_product.age,
-                                                     student=default_product.student,
-                                                     income=default_product.income)
+                print(f'Creating product {default_product["name"]}...')
+                new_product = Product.objects.create(name=default_product['name'],
+                                                     age=default_product['age'],
+                                                     student=default_product['student'],
+                                                     income=default_product['income'])
                 new_product.save()
         else:
             print('Products already exist.')
